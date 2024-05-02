e88ca313eaaab5544d115299916638ece2ec4b1a
unknown <HP@LAPTOP-P6N3BPAQ.com>
2024-05-02 16:59:12 +0700

    tugas 8

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String helo = "mangcoding nih boss";
-        String name = "mangcoding nih boss";
-        String hai = "mangcoding nih boss";
         String stringOri = "Welcome to Mangcoding";
         String stringLower = stringOri.toLowerCase();
-        String stringAsli = "Welcome to Mangcoding";
-        String stringAtas = stringOri.toUpperCase();
+        String stringUpper = stringOri.toUpperCase();
         String string1 = "Helo How Are You";
         String GantiKarakter = string1.replace("H", "M");
         String GantiKalimat = string1.replace("Helo", "hei dude");
@@ -17,12 +14,12 @@ public class App {
 
 
         System.out.println("hasilnya adalah : " + helo.substring(2, 11));
-        System.out.println("hasilnya adalah : " + name.length());
-        System.out.println("hasilnya adalah : " + hai.charAt(7));
+        System.out.println("hasilnya adalah : " + helo.length());
+        System.out.println("hasilnya adalah : " + helo.charAt(7));
         System.out.println("sebelum digunakan method tolowercase : " + stringOri);
         System.out.println("sesudah digunakan method tolowercase  : " + stringLower);
-        System.out.println("sebelum digunakan method tolowercase : " + stringAsli);
-        System.out.println("sesudah digunakan method tolowercase  : " + stringAtas);
+        System.out.println("sebelum digunakan method touppercase : " + stringOri);
+        System.out.println("sesudah digunakan method touppercase : " + stringUpper);
         System.out.println("untuk mengganti karakter : " + GantiKarakter);
         System.out.println("untuk mengganti kalimat : " + GantiKalimat);
         System.out.println("Hasilnya :" + nama.contains("NusaPutra"));
@@ -37,7 +34,6 @@ public class App {
         System.out.println("Hasilnya :" + string1.endsWith("you"));
         System.out.println("Hasilnya :" + string1.endsWith("o"));
         System.out.println("Hasilnya :" + string1.endsWith("y"));
-
     }
 }
 
